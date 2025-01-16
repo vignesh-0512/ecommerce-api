54e75e57c05308382850ebaae8605c77421832d5
Vignesh T <115525353+vignesh-0512@users.noreply.github.com>
2025-01-16 12:53:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '12345')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
     MYSQL_DB = os.getenv('MYSQL_DB', 'ecommerce')
